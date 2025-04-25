192747345706718d2281eaf1bc1f4b1aba3388cf
dubinbin <szdubinbin@gmail.com>
2025-04-25 10:41:39 +0800

    feat: delete queue

--- src/queue/task.queue.ts
+++
@@ -1,8 +0,0 @@
-import { Processor, IProcessor } from '@midwayjs/bull';
-
-@Processor('render')
-export class TaskProcessor implements IProcessor {
-  async execute(data: any) {
-    console.log(data);
-  }
-}
